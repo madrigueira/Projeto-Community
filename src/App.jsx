04d586dd9682a7ae1964000332d0d0811b729492
madrigueira <mndias29@gmail.com>
2024-04-06 15:18:17 -0300

    começando de novo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./index.scss"
+import {HeaderMain} from "./components/HeaderMain"
+
+export const App = () => {
+  return (
+    <div className='App'>
+      <HeaderMain />
+    </div>
+  )
+}
---
+++ src/components/HeaderMain/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./index.scss"
+
+export const HeaderMain = () => {
+  return (
+    <div className='HeaderMain'>
+      <div className="circle">
+        <h1>Projeto Community</h1>
+      </div>
+      <div className="container">
+        <button>Eps assistidos: 00</button>
+        <div className="profile-info">
+          <img src="https://media.comicbook.com/wp-content/uploads/2013/12/winger-guarantee.png" alt="Profile Pic" />
+          <h5>Matheus Negrini</h5>
+        </div>
+        <button>Logar</button>
+      </div>
+    </div>
+  )
+}
