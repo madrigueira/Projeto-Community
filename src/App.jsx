3633a987459671176d3be7da12f75dc844680ced
madrigueira <mndias29@gmail.com>
2024-05-01 17:32:39 -0300

    tapourra

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./index.scss"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
 import { HeaderMain } from './components/HeaderMain'
+import { Create } from './pages/Create'
 
 export const App = () => {
   return (
     <div className='App'>
       <Router>
         <Routes>
-          <Route path="/" element={<HeaderMain/>}></Route>
+          <Route path="/" element={<LoginPage/>}></Route>
+          <Route path="/create" element={<Create/>}></Route>
         </Routes>
       </Router>
     </div>
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { useEffect, useState } from "react";
 
 export const LoginPage = () => {
+  const [records, setRecords] = useState([]);
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:5050/record/`);
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        console.error(message);
+        return;
+      }
+      const records = await response.json();
+      setRecords(records);
+    }
+    getRecords();
+    return;
+  }, [records.length]);
+
+  // This method will delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:5050/record/${id}`, {
+      method: "DELETE",
+    });
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
+
+  // This method will map out the records on the table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <h2 key={record._id}>{record.name}</h2>
+      );
+    });
+  }
+
+
+
   return (
     <div className='LoginPage'>
       <h1>LoginPage</h1>
+      {recordList()}
     </div>
   )
 }
